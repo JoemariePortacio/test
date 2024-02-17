5c83496775b1b73bcc6e85ab45c432482520ec93
JoemariePortacio <joemarieportacio00@gmail.com>
2024-02-17 10:46:10 +0800

    update home

--- resources/views/settings/home.blade.php
+++ resources/views/settings/home.blade.php
@@ -6,15 +6,15 @@
         <h3 class="w-full mx-auto text-center text-4xl font-semibold"> 
             Setting
         </h3>
-        <div class="grid grid-cols-1 md:grid-cols-3 gap-10 items-center pt-10 "> 
+        <div class="grid grid-cols-1 md:grid-cols-3 gap-4 items-center pt-10 "> 
             <a href="{{route('settings.account')}}" class="p-10 rounded-lg items-center text-center shadow-xl hover:bg-slate-300   hover:text-gray-700">
                 <i class="text-6xl fa-solid fa-users"></i>
-                <div class="p-2 items-center text-center">
+                <div class="items-center text-center">
                     <h2 class="p2">Account</h2>
                     <p class="p-2">Manage Employee Account</p>
                 </div>
             </a>
-            <a href="{{route('settings.activitylog')}}" class="p-10 items-center rounded-lg text-center shadow-xl hover:bg-slate-300   hover:text-gray-700">
+            <a href="{{route('settings.activitylog')}}" class="p-8 items-center rounded-lg text-center shadow-xl hover:bg-slate-300   hover:text-gray-700">
                 <i class="text-6xl fa-solid fa-address-book"></i>
                 <div class="items-center text-center">
                     <h2 class="p-2">Activity Log</h2>
@@ -22,8 +22,8 @@
                 </div>
             </a>
             <a href="{{route('settings.permissions')}}" class="p-10 rounded-lg items-center text-center shadow-xl hover:bg-slate-300   hover:text-gray-700">
-                <i class="text-6xl fa-solid fa-users"></i>
-                <div class="p-2 items-center text-center">
+                <i class="text-6xl fa-solid fa-lock"></i>
+                <div class=" items-center text-center">
                     <h2 class="p2">Permission</h2>
                     <p class="p-2">Manage Users Control</p>
                 </div>
