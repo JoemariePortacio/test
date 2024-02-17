00900397a75f04a63205a3a3c523595a691fb72d
JoemariePortacio <joemarieportacio00@gmail.com>
2024-02-17 10:47:53 +0800

    update home again

--- resources/views/settings/home.blade.php
+++ resources/views/settings/home.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
 
 
-<div class="h-fit m-5">
+<div class="h-full m-5">
     <div class="pt-24 w-full h-full flex flex-col space-y-10 justify-center items-center">
         <h3 class="w-full mx-auto text-center text-4xl font-semibold"> 
             Setting
